9561696d81f5f42aa1d9ca8186ef125a8adc9a52
Vladimir <vladimir.englishbox@gmail.com>
2018-03-12 19:59:43 +0300

    Added olympiad task

---
+++ olympiad_tasks/2012.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <chrono>
+
+using namespace std::chrono_literals;
+
+auto availableTime = 4h;
+auto timeForOneTask = 45min;
+
+constexpr int canBeSolvedTasks()
+{
+	return availableTime / timeForOneTask;
+}
+
+void checkRange(int number)
+{
+	if (!(1 <= number) || !(number <= 11))
+		throw std::invalid_argument("received value not within a valid range");
+}
+
+int main(int argc, char const *argv[])
+{
+	try
+	{
+		int solvedTasks;
+		std::cin >> solvedTasks;
+		checkRange(solvedTasks);
+
+		int overallTasks = 12;
+
+		if(solvedTasks + canBeSolvedTasks() >= overallTasks)
+			std::cout << "YES" << std::endl;
+		else
+			std::cout << "NO" << std::endl;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	return 0;
+}
