13224610be895aca22ded0d31386cf2fdbd02474
vladimir.englishbox@gmail.com <avovana@gmail.com>
2018-03-19 08:28:53 +0400

    Refactored code - added library to make project look better

--- lesson2_bin_id/bin_id.cpp
+++ lesson2_bin_id/bin_id.cpp
@@ -1,29 +1,7 @@
+// Реализовать constexpr функцию bin_id - определения ближайшей большей степени двойки 
 #include <bits/stdc++.h>
 
-// Реализовать constexpr функцию bin_id - определения ближайшей большей степени двойки 
-namespace
-{
-	#define b_01 1
-
-	constexpr bool additionalBitsAbsent(size_t value)
-	{
-		return (value != b_01) ? (
-								  ((value & b_01) != b_01) ? true : false
-								 )
-								 : true;
-	}
-
-	constexpr size_t bin_id(size_t val, bool pure_power = true, size_t degree = 0)
-	{
-		return val ? (
-					  (val >> 1) ?															
-								   pure_power ? 
-											    degree + 1 + bin_id(val >> 1, additionalBitsAbsent(val))
-											  : degree + 1 + bin_id(val >> 1, false)
-								 : pure_power ? 0 : 1
-					 )
-					 : 0;
-	}	
+#include "lib.h"
 
 	static_assert(bin_id(0) == 0, "bin_id doesn't work");
 	static_assert(bin_id(1) == 0, "bin_id doesn't work");
@@ -41,7 +19,6 @@ namespace
 	static_assert(bin_id(1024*1024*1024-127) == 30, "bin_id doesn't work");
 	static_assert(bin_id(1024*1024*1024) == 30, "bin_id doesn't work");
 	static_assert(bin_id(1024*1024*1024+127) == 31, "bin_id doesn't work"); 
-}
 
 int main()
 {
---
+++ lesson2_bin_id/lib.h
@@ -0,0 +1,5 @@
+	#include "lib.h"
+  
+  constexpr bool additionalBitsAbsent(size_t value);
+
+	constexpr size_t bin_id(size_t val, bool pure_power = true, size_t degree = 0);
