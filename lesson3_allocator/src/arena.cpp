22ce172cb9428ece75cfb20863541070818a7cc9
Vladimir <vladimir.englishbox@gmail.com>
2018-04-04 21:17:47 +0300

    Fixed debug output

--- lesson3_allocator/src/arena.cpp
+++ lesson3_allocator/src/arena.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#define DEBUG 0
+
 class Arena
 {
     unsigned char * const data;
@@ -12,7 +14,9 @@ public:
     , size(s)
     , offset(0)
     {
+		#ifdef DEBUG
         std::cout << "arena[" << this << "] of size " << size << " created.\n";
+		#endif
     }
 
     Arena(Arena const &) = delete;
@@ -20,18 +24,25 @@ public:
 
     ~Arena()
     {
+		#ifdef DEBUG
         std::cout << "arena[" << this << "] destroyed; final fill level was: " << offset << "\n";
+		#endif
         ::operator delete(data);
     }
 
     void * allocate(std::size_t n, std::size_t a)
     {
+		#ifdef DEBUG
         std::cout << __PRETTY_FUNCTION__ << std::endl;
         std::cout << " " << "size is:  " << n << std::endl;
         std::cout << " " << "align is: " << a << std::endl;
+		#endif
+		
         offset = (offset + a - 1) / a * a;
 
+		#ifdef DEBUG
         std::cout << "arena[" << this << "] allocating " << n << " bytes at offset " << offset << ".\n";
+		#endif
 
         if (offset + n > size)
         {
@@ -45,6 +56,8 @@ public:
 
     void deallocate(void *, std::size_t n)
     {
+		#ifdef DEBUG
         std::cout << "arena[" << this << "] may deallocate " << n << " bytes.\n";
+		#endif
     }
 };
